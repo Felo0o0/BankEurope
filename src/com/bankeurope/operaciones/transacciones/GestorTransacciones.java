db60dec0a71ec99d848464c2b28d7f6360307e89
Felo0o0 <feli.barahona@duocuc.cl>
2025-06-08 12:40:28 -0400

    Update GestorTransacciones.java

--- src/com/bankeurope/operaciones/transacciones/GestorTransacciones.java
+++ src/com/bankeurope/operaciones/transacciones/GestorTransacciones.java
@@ -3,11 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-/**
- * 
- * @author felix
- */
-
 package com.bankeurope.operaciones.transacciones;
 
 import com.bankeurope.modelo.cuenta.Cuenta;
@@ -15,11 +10,17 @@ import java.util.Date;
 
 /**
  * Clase para gestionar las transacciones bancarias
+ * Proporciona metodos para registrar y procesar diferentes tipos de transacciones
+ * 
+ * @author Bank Europe Development Team
+ * @version 1.0
  */
 public class GestorTransacciones {
     
     /**
      * Registra una transaccion de deposito
+     * Valida el monto y realiza el deposito en la cuenta especificada
+     * 
      * @param cuenta La cuenta donde se realiza el deposito
      * @param monto El monto del deposito
      * @return true si la transaccion se completa con exito
@@ -37,6 +38,8 @@ public class GestorTransacciones {
     
     /**
      * Registra una transaccion de giro
+     * Valida el monto y realiza el giro desde la cuenta especificada
+     * 
      * @param cuenta La cuenta de donde se realiza el giro
      * @param monto El monto del giro
      * @return true si la transaccion se completa con exito
@@ -56,6 +59,11 @@ public class GestorTransacciones {
     
     /**
      * Metodo privado para registrar los detalles de una transaccion
+     * Guarda informacion sobre el tipo, cuenta, monto y fecha de la transaccion
+     * 
+     * @param tipo Tipo de transaccion (Deposito, Giro, etc.)
+     * @param cuenta Cuenta involucrada en la transaccion
+     * @param monto Monto de la transaccion
      */
     private void registrarTransaccion(String tipo, Cuenta cuenta, double monto) {
         Date fecha = new Date();
