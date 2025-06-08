041e50f2b2f1444c6d3cff704f6658828b7d5311
Felo0o0 <feli.barahona@duocuc.cl>
2025-06-08 12:37:22 -0400

    Update InfoCliente.java

--- src/com/bankeurope/modelo/InfoCliente.java
+++ src/com/bankeurope/modelo/InfoCliente.java
@@ -3,22 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-/**
- *
- * @author felix
- */
-
 package com.bankeurope.modelo;
 
 /**
- * Interfaz que define métodos para mostrar información del cliente
- * Las clases que implementen esta interfaz deben proporcionar implementaciones
- * para mostrar información del cliente
+ * Interfaz que define el comportamiento para mostrar informacion detallada del cliente
+ * Esta interfaz es implementada por las clases que necesitan mostrar informacion
+ * especifica del cliente asociado a diferentes tipos de cuentas bancarias.
+ * 
+ * @author Bank Europe Development Team
+ * @version 1.0
  */
 public interface InfoCliente {
+    
     /**
-     * Muestra información del cliente
-     * Implementación con los datos del cliente
+     * Metodo que muestra informacion detallada del cliente
+     * Cada clase que implemente esta interfaz debe proporcionar su propia
+     * implementacion para mostrar la informacion relevante del cliente
+     * segun el contexto especifico.
      */
     void mostrarInformacionCliente();
 }
